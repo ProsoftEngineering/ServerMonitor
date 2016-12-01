1a82f163b28949bf7e5a9e2a52f2fca54ba3b1cd
Kevin Wojniak <kainjow@users.noreply.github.com>
2016-11-30 20:22:07 -0800

    Follow redirects

--- src/http_curl.cpp
+++ src/http_curl.cpp
@@ -32,6 +32,7 @@ bool HttpHead(const std::string& url, unsigned timeout, std::string& errorMessag
     HANDLE_CURL_CODE(curl_easy_setopt(handle.value, ::CURLOPT_NOBODY, 1L)); // HEAD request
     HANDLE_CURL_CODE(curl_easy_setopt(handle.value, ::CURLOPT_URL, url.c_str()));
     HANDLE_CURL_CODE(curl_easy_setopt(handle.value, ::CURLOPT_TIMEOUT, static_cast<long>(timeout)));
+    HANDLE_CURL_CODE(curl_easy_setopt(handle.value, ::CURLOPT_FOLLOWLOCATION, 1L));
     HANDLE_CURL_CODE(::curl_easy_perform(handle.value));
     long http_code = 0;
     HANDLE_CURL_CODE(curl_easy_getinfo(handle.value, ::CURLINFO_RESPONSE_CODE, &http_code));
