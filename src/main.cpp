ProsoftEngineering/ServerMonitor

commit 2e1e08084a14993ae2ca12dffaeaf906be89e87c

Add error message to output status

src/main.cpp
@@ -568,7 +568,12 @@ public:
                 std::cout << name << ": DOWN - " << monitor->errorMessage();
             }
             std::cout << " (" << monitor->duration() << " ms)" << std::endl;
-            status[name] = result;
+            json server_info;
+            server_info["status"] = result;
+            if (!result) {
+                server_info["error"] = monitor->errorMessage();
+            }
+            status[name] = server_info;
             
             const auto status_prev_it = status_prev.find(name);
             if (status_prev_it != status_prev.end() && status_prev_it->is_boolean() && status_prev_it->get<bool>() != result) {

status.html
@@ -27,9 +27,10 @@
     <script>
     $.getJSON('status.json', function(data) {
       var table = $('table');
-      $.each(data, function(key, value) {
+      $.each(data, function(key, info) {
         var name_td = $('<td></td>').text(key);
-        var status_td = $('<td></td>').text(value ? 'Up' : 'Down');
+        var value = info['status'];
+        var status_td = $('<td></td>').text(value ? 'Up' : info['error']);
         var tr = $('<tr></tr>').append(name_td, status_td);
         tr.addClass(value ? 'success' : 'danger');
         table.append(tr);

commit de518b77a2a5df930583e346d31873a1e6afeb7a

Fix up/down handling broken in 2e1e080. Change output to be an array. In status.html show servers with errors on the top.

src/main.cpp
@@ -563,7 +563,7 @@ public:
         
         for (const auto& server : servers) {
             const auto& monitor = server.monitor();
-            const auto name = server.name();
+            const auto& name = server.name();
             const bool result = server.result();
             if (result) {
                 std::cout << name << ": UP";
@@ -572,19 +572,30 @@ public:
             }
             std::cout << " (" << monitor->duration() << " ms)" << std::endl;
             json server_info;
+            server_info["name"] = name;
             server_info["status"] = result;
             if (!result) {
                 server_info["error"] = monitor->errorMessage();
             }
-            status[name] = server_info;
+            status.push_back(server_info);
             
-            const auto status_prev_it = status_prev.find(name);
-            if (status_prev_it != status_prev.end() && status_prev_it->is_boolean() && status_prev_it->get<bool>() != result) {
-                std::cout << "  Handle " << (result ? "UP" : "DOWN") << std::endl;
-                if (!server.action().empty()) {
-                    const auto action_iter = actions.find(server.action());
-                    if (action_iter != actions.end()) {
-                        action_iter->second->run(server);
+            const auto& status_prev_it = std::find_if(status_prev.begin(), status_prev.end(), [&name](const json& obj) {
+                return obj["name"] == name;
+            });
+            if (status_prev_it != status_prev.end()) {
+                if (!status_prev_it->is_object()) {
+                    std::cout << "WARNING: Invalid status JSON element for \"" << name << "\"" << std::endl;
+                    continue;
+                }
+                const auto& json_obj = *status_prev_it;
+                const auto& json_status = json_obj.find("status");
+                if (json_status != json_obj.end() && json_status->get<bool>() != result) {
+                    std::cout << "  Handle " << (result ? "UP" : "DOWN") << std::endl;
+                    if (!server.action().empty()) {
+                        const auto action_iter = actions.find(server.action());
+                        if (action_iter != actions.end()) {
+                            action_iter->second->run(server);
+                        }
                     }
                 }
             }

status.html
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   </head>
   <body>
+
     <div class="container">
       <h1>Server Monitor</h1>
       <table class="table table-bordered table-condensed">
@@ -27,8 +28,13 @@
     <script>
     $.getJSON('status.json', function(data) {
       var table = $('table');
-      $.each(data, function(key, info) {
-        var name_td = $('<td></td>').text(key);
+      var dataSorted = data.sort(function(a, b) {
+        var a_status = a['status'];
+        var b_status = b['status'];
+        return a_status == b_status ? 0 : (a_status ? 1 : -1);
+      });
+      $.each(dataSorted, function(index, info) {
+        var name_td = $('<td></td>').text(info['name']);
         var value = info['status'];
         var status_td = $('<td></td>').text(value ? 'Up' : info['error']);
         var tr = $('<tr></tr>').append(name_td, status_td);